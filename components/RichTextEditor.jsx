9a9df04c67fb50f09044f017ff96998d7bbf4a32
Sayandeep <sdroy001@gmail.com>
2025-12-21 22:38:31 +0530

    added rich text editor

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -9,6 +9,33 @@ const RichTextEditor = ({
 }) => {
   return (
     <View style={{minHeight: 285}}>
+        <RichToolbar
+            actions={[
+                actions.setStrikethrough,
+                actions.removeFormat,
+                actions.setBold,
+                actions.setItalic,
+                actions.insertOrderedList,
+                actions.blockquote,
+                actions.alignLeft,
+                actions.alignCenter,
+                actions.alignRight,
+                actions.code,
+                actions.line,
+                actions.heading1,
+                actions.heading4
+            ]}
+            iconMap={{
+                [actions.heading1] : ({tintColor}) => <Text style={{color:tintColor}}>H1</Text>,
+                [actions.heading4] : ({tintColor}) => <Text style={{color:tintColor}}>H4</Text>,
+            }}
+            style={styles.richBar}
+            flatContainerStyle={styles.flatStyle}
+            selectedIconTint={theme.colors.primaryDark}
+            editor={editorRef}
+            disabled={false}
+        />
+
         <RichEditor
             ref={editorRef}
             containerStyle={styles.rich}
@@ -33,9 +60,7 @@ const styles = StyleSheet.create({
         minHeight: 240,
         flex: 1,
         borderWidth: 1.5,
-        borderTopWidth: 1.5,
-        borderTopLeftRadius: theme.radius.xl,
-        borderTopRightRadius: theme.radius.xl,
+        borderTopWidth: 0,
         borderBottomLeftRadius: theme.radius.xl,
         borderBottomRightRadius: theme.radius.xl,
         borderColor: theme.colors.gray,
@@ -46,6 +71,7 @@ const styles = StyleSheet.create({
         color: theme.colors.textDark,
         placeholderColor: 'gray',
     },
+
     flatStyle:{
         paddingHorizontal: 8,
         gap: 3,
