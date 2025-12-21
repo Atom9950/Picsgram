b39809d7632667329f892d32662226987edcd968
Sayandeep <sdroy001@gmail.com>
2025-12-21 19:54:34 +0530

    removed the rich editor

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -9,33 +9,6 @@ const RichTextEditor = ({
 }) => {
   return (
     <View style={{minHeight: 285}}>
-        <RichToolbar
-            actions={[
-                actions.setStrikethrough,
-                actions.removeFormat,
-                actions.setBold,
-                actions.setItalic,
-                actions.insertOrderedList,
-                actions.blockquote,
-                actions.alignLeft,
-                actions.alignCenter,
-                actions.alignRight,
-                actions.code,
-                actions.line,
-                actions.heading1,
-                actions.heading4
-            ]}
-            iconMap={{
-                [actions.heading1] : ({tintColor}) => <Text style={{color:tintColor}}>H1</Text>,
-                [actions.heading4] : ({tintColor}) => <Text style={{color:tintColor}}>H4</Text>,
-            }}
-            style={styles.richBar}
-            flatContainerStyle={styles.flatStyle}
-            selectedIconTint={theme.colors.primaryDark}
-            editor={editorRef}
-            disabled={false}
-        />
-
         <RichEditor
             ref={editorRef}
             containerStyle={styles.rich}
@@ -60,7 +33,9 @@ const styles = StyleSheet.create({
         minHeight: 240,
         flex: 1,
         borderWidth: 1.5,
-        borderTopWidth: 0,
+        borderTopWidth: 1.5,
+        borderTopLeftRadius: theme.radius.xl,
+        borderTopRightRadius: theme.radius.xl,
         borderBottomLeftRadius: theme.radius.xl,
         borderBottomRightRadius: theme.radius.xl,
         borderColor: theme.colors.gray,
